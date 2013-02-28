cb073f91b43b41619a5ae1ca7532cb2ac7e24eac
Lionel Dong <1950195@qq.com>
2013-02-28 22:47:34 +0800

    modify js

--- selectorTool/selectorTool_v1.js
+++ selectorTool/selectorTool_v1.js
@@ -2,95 +2,127 @@ if (!window.selectorTool.hasJQuery) {
     jQuery.noConflict();
 }
 (function(window, document, $, undefined) {
-    var getSeletor = function(dCurrent) {
-            //First, get full selector whole tagname and classname and id, then use '>' contact each other order by parent to child.
-            //Of course, it's not correct.
-            var strSelector = (function(node) {
-                    var sParentSelector = (node.prop('id') && node.prop('id').indexOf('yui_') !== 0) || node.parent()[0] === document ? '' : arguments.callee(node.parent()),
-                        sNodeTagName = node.prop('tagName').toLowerCase(),
-                        sNodeId = node.prop('id') && node.prop('id').indexOf('yui_') !== 0 ? '#' + node.prop('id') : '',
-                        sNodeClass = node.prop('class') ? (function(s) {
-                                var i = 0,
-                                    arr = s.split(' ');
-                                for (s = '', len = arr.length; i < len; i++) {
-                                    arr[i] = arr[i].trim();
-                                    if (arr[i] && arr[i].indexOf('selector_tool_') !== 0 && arr[i] !== 'current') {
-                                        s += '.' + arr[i];
-                                    }
-                                }
-                                return s;
-                            })(node.prop('class')) : '';
-                    return sParentSelector + '>'  + sNodeTagName + sNodeId + sNodeClass;
-                })(dCurrent).slice(1),
-                arrSelector;
-            //Second, some node selectors map more than one element, it must be add ':nth-of-type(n)'. 
-            if ($(strSelector).length > 1) {
-                var arrTemp = strSelector.split('>');
-                $(arrTemp).each(function(k, v) {
-                    var strTemp = arrTemp.slice(0, k).join('>');
-                    strTemp = strTemp ? strTemp += '>' : '';
-                    if ($(strTemp + v).length === 1) {
-                        return true;
-                    }
-                    v = v.replace(/[\.:#].*$/, '');
-                    arrTemp[k] = v + ':nth-of-type(' + ($(strTemp + v).index(dCurrent.closest(strTemp + v)) + 1) + ')';
-                });
-                strSelector = arrTemp.join('>');
+    //为jQuery对象扩展equals方法
+    //功能：遍历判断两个jQuery的对象是否一致
+    //.equals(selector) Type: Selector
+    //.equals(element) Type: Elements or jQuery
+    $.fn.equals = function(elem) {
+        if (!elem) {
+            return false;
+        }
+        if (typeof elem === 'string') {
+            elem = $(elem);
+        }
+        var len = this.length;
+        if (this.length !== elem.length) {
+            return false;
+        }
+        for (var i = 0; i < len; i++) {
+            if (this[i] !== elem[i]) {
+                return false;
             }
-            //Third, some '>' are not necessary, we can remove them.
-            strSelector = (function(str, from) {
-                var index = str.indexOf('>', from),
-                    strTemp;
-                if (index === -1) {
-                    return str;
-                }
-                strTemp = str.slice(0, index) + ' ' + str.slice(index + 1);
-                if ($(strTemp).length === 1 && dCurrent.index(strTemp) === 0) {
-                    str = strTemp;
-                }
-                return arguments.callee(str, index + 1);
-            })(strSelector, 0);
-            //At last, compress selectors.
-            arrSelector = strSelector.replace(/(>)/g, ' $1').split(' ');
-            $(arrSelector).each(function(k, v) {
-                var arrTemp = arrSelector.slice(),
-                    suffixB = '',
-                    suffixE = '';
-                //if this node selector is not necessary, we can remove it.
-                if (k > 0) {
-                    arrTemp[k] = '';
-                    if ($(arrTemp.join(' ')).length === 1 && dCurrent.index(arrTemp.join(' ')) === 0) {
-                        arrSelector[k] = arrTemp[k];
-                        return true;
-                    } else {
-                        arrTemp[k] = v;
+        }
+        return true;
+    };
+    //配置
+    var config = {
+        //筛选id、className
+            filter: {
+                id: function(id) {
+                        return id.replace(/(^yui_.*$)/, '');
+                    },
+                className: function(className) {
+                        return className.replace(/(selector_tool_\S+|yom-\S+|yui3-\S+|current|selected)(\s+|$)/g, '$2');
+                    }
+            }
+        },
+        //得到当前node的id，经过过滤，如果非空字符串，则返回 ‘#’ + id；否则返回空字符串
+        getNodeId = function(node) {
+            var nodeId = config.filter.id(node.prop('id'));
+            return nodeId ? '#' + nodeId : '';
+        },
+        //得到当前node的className，经过过滤，拼接所有className，返回 ‘.’ + className1 + ‘.’ + className2 + ... + ‘.’ + classNameN
+        getNodeClassName = function(node) {
+            return config.filter.className(node.prop('class')).trim().replace(/\s*(\S+)/g, '.$1');
+        },
+        //返回完整的当前节点的class：tagName + ‘#’ + id + ‘.’ + className1 + ‘.’ + className2 + ... + ‘.’ + classNameN
+        getSingleNodeClass = function(node) {
+            var tagName = node.prop('tagName').toLowerCase();
+            return tagName === 'html' ? '' : tagName + getNodeId(node) + getNodeClassName(node);
+        },
+        //返回最终的selector数组
+        //在这里各种计算，最终留下最合适的selector
+        getArrSelector = function(node, arrSelector) {
+            var fullSelector,
+                fullSelectorTemp,
+                dTemp,
+                arrTemp,
+                singleNodeClass,
+                parentNode,
+                siblingNodes,
+                arrLength;
+            //初始化 arrSelector
+            if (arrSelector === undefined) {
+                arrSelector = [];
+            } else if (arrSelector.length > 0) {
+                fullSelector = arrSelector.join('');
+                //1. 试试用 ‘>’ 和 ‘ ’ 的效果是不是一样的：相同则用 ‘ ’ 取代 ‘>’
+                fullSelectorTemp = fullSelector.replace(/>/, ' ');
+                dTemp = $(fullSelector);
+                if (arrSelector.length > 1 && dTemp.equals(fullSelectorTemp)) {
+                    arrSelector[1] = arrSelector[1].replace(/>/, ' ');
+                    if (arrSelector.length > 2) {
+                        fullSelector = fullSelectorTemp;
+                //2. 试试干掉第二个，有没有区别：相同则干掉
+                        fullSelectorTemp = fullSelectorTemp.replace(/^([^>\s]+\s)([^>\s]+\s)/, '$1');
+                        if (fullSelector !== fullSelectorTemp && dTemp.equals(fullSelectorTemp)) {
+                            arrSelector.splice(1, 1);
+                        }
                     }
                 }
-                //one node selector maybe has tagname and id and more than one classname,
-                //so, we will make it more concise.
-                if (v.indexOf('>') === 0) {
-                    suffixB = '>';
-                    arrTemp[k] = arrTemp[k].slice(1);
-                }
-                if (v.indexOf(':') > -1) {
-                    arrTemp[k] = arrTemp[k].slice(0, arrTemp[k].indexOf(':'));
-                    suffixE = v.slice(v.indexOf(':'));
-                }
-                arrSelector[k] = (function(arr) {
-                    if (arr.length > 1) {
+                //3. 试试看排在最前面的选择器，能不能再优化，比如去掉tagName，去掉其中一部分className，甚至去掉id
+                fullSelectorTemp = fullSelector.replace(/^([^>\s]*)([>\s])/, '$2');
+                arrTemp = arrSelector[0].replace(/([#\.])/g, ' $1').trim().split(' ');
+                if (arrTemp.length > 1) {
+                    arrSelector[0] = (function(arr) {
+                        if (arr.length === 1) {
+                            return arr;
+                        }
                         for (var i = 0; i < arr.length; i++) {
                             var _arrTemp = arr.slice();
                             _arrTemp.splice(i, 1);
-                            arrTemp[k] = suffixB + _arrTemp.join('') + suffixE;
-                            if ($(arrTemp.join(' ')).length === 1 && dCurrent.index(arrTemp.join(' ')) === 0) {
+                            if (dTemp.equals(_arrTemp.join('') + fullSelectorTemp)) {
                                 return arguments.callee(_arrTemp);
                             }
                         }
-                    }
-                    return suffixB + arr.join('') + suffixE;
-                })(arrTemp[k].replace(/([#\.])/g, ' $1').trim().split(' '));
-            });
-            return arrSelector.join(' ').replace(/\s+([\s>])/g, '$1');
+                        return arr;
+                    })(arrTemp).join('');
+                }
+                //大功告成
+                if (dTemp.equals(node)) {
+                    return arrSelector;
+                }
+            }
+            singleNodeClass = getSingleNodeClass(node);
+            //如果是空字符串，那说明已经追溯到 html了，那没必要再找下去了
+            if (!singleNodeClass) {
+                return arrSelector;
+            }
+            parentNode = node.parent();
+            siblingNodes = parentNode.children(singleNodeClass);
+            //4. 有时候，通过tagName、id和class根本没办法准确定位到这个node，只好动用杀手锏 ‘:nth-of-type(n)’，n >= 1
+            if (siblingNodes.length > 1) {
+                singleNodeClass = singleNodeClass.replace(/^([^\.#]*)([\.#].*)?$/, '$1:nth-of-type(' + (node.index() + 1) + ')');
+            }
+            arrLength = arrSelector.unshift(singleNodeClass);
+            //5. 默认是用 '>' 连接的
+            if (arrLength > 1) {
+                arrSelector[1] = '>' + arrSelector[1];
+            }
+            return arguments.callee(parentNode, arrSelector);
+        },
+        getSeletor = function(dCurrent) {
+            return getArrSelector(dCurrent).join('');
         },
         getToolItem = function(selector) {
             var item = [];
